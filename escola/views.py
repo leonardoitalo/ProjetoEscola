8fa29a694f5b82a08d77f84e3f801f70c7cbf725
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-01-06 02:55:31 -0300

    Adicionando cabeçalho location

--- escola/views.py
+++ escola/views.py
@@ -1,6 +1,8 @@
 from rest_framework import viewsets, generics
+from rest_framework import status
 from escola.models import Aluno, Curso, Matricula
 from escola.serializer import AlunoSerializer, CursoSerializer, MatriculaSerializer, ListaMatriculasAlunoSerializer, ListaAlunosMatriculadosSerializer, AlunoSerializerV2
+from rest_framework.response import Response
 
 
 class AlunosViewSet(viewsets.ModelViewSet):
@@ -18,6 +20,15 @@ class CursosViewSet(viewsets.ModelViewSet):
     queryset = Curso.objects.all()
     serializer_class = CursoSerializer
     
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            response = Response(serializer.data, status=status.HTTP_201_CREATED)
+            id = str(serializer.data['id'])
+            response['Location'] = request.build_absolute_uri() + id
+            return response
+        
 class MatriculaViewSet(viewsets.ModelViewSet):
     """Listando todas as matrículas"""
     queryset = Matricula.objects.all()
