4fb198b89001a539e3d83924efde8c55566037ca
leonardoitalo <131911745+leonardoitalo@users.noreply.github.com>
2024-01-08 02:02:52 -0300

    Criando teste para GET e POST

--- escola/test/tests_cursos.py
+++ escola/test/tests_cursos.py
@@ -1,12 +1,13 @@
 from rest_framework.test import APITestCase
 from escola.models import Curso
 from django.urls import reverse
+from rest_framework import status
 
 
 class CursosTestCase(APITestCase):
 
     def setUp(self):
-        self.list_url = reverse("Curso-list")
+        self.list_url = reverse("Cursos-list")
 
         self.curso_1 = Curso.objects.create(
             codigo_curso="CTT1", descricao="Curso teste 1", nivel="B"
@@ -16,5 +17,21 @@ class CursosTestCase(APITestCase):
         )
         
 
-    def test_falhador(self):
-        self.fail("Falha proposital")
+    # def test_falhador(self):
+    #     self.fail("Falha proposital")
+        
+    def test_requisicao_get_para_lista_cursos(self):
+        """Teste para verificar a requisicao GET para listar os cursos"""
+        response = self.client.get(self.list_url)
+        self.assertEquals(response.status_code, status.HTTP_200_OK)
+
+
+    def test_requisicao_post_para_criar_curso(self):
+         """Teste para verificar a requisicao POST para criar um curso"""
+         data = {
+             "codigo_curso": "CTT3",
+             "descricao": "Curso teste 3",
+             "nivel": "A"
+         }
+         response = self.client.post(self.list_url, data=data)
+         self.assertEquals(response.status_code, status.HTTP_201_CREATED)
